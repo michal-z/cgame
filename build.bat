0735746f344ea10ab9cdee3032a3fd317d526fd0
Michal Ziulek <michalziulek@gmail.com>
2024-07-29 14:34:15 +0200

    Update

--- build.bat
+++ build.bat
@@ -1,24 +1,37 @@
-@echo off
-setlocal enableextensions enabledelayedexpansion
+@ECHO off
+SETLOCAL enableextensions enabledelayedexpansion
 
-set CONFIG=D
-set C_FLAGS=/std:c17 /I. /experimental:c11atomics /GR- /nologo /Gm- /WX /Wall /wd4191 /wd4820 /wd4255 /wd5045
+SET NAME="cgame"
 
-if %CONFIG%==D set C_FLAGS=%C_FLAGS% /GS /Zi /Od /D"_DEBUG" /MTd /RTCs
-if %CONFIG%==R set C_FLAGS=%C_FLAGS% /O2 /Gy /MT /D"NDEBUG" /Oi /Ot /GS-
+SET CONFIG=D
+SET C_FLAGS=/std:c17 /I. /experimental:c11atomics /GR- /nologo /Gm-^
+ /WX /Wall /wd4191 /wd4820 /wd4255 /wd5045^
+ /I.^
+ /I"deps"^
+ /I"deps/d3d12"
 
-set LINK_FLAGS=/INCREMENTAL:NO /NOLOGO
-if %CONFIG%==D set LINK_FLAGS=%LINK_FLAGS% /DEBUG:FULL
-if %CONFIG%==R set LINK_FLAGS=%LINK_FLAGS%
+IF %CONFIG%==D set C_FLAGS=%C_FLAGS% /GS /Zi /Od /D"_DEBUG" /MTd /RTCs
+IF %CONFIG%==R set C_FLAGS=%C_FLAGS% /O2 /Gy /MT /D"NDEBUG" /Oi /Ot /GS-
 
-if exist *.obj del *.obj
-if exist *.pdb del *.pdb
-if exist *.exe del *.exe
+SET LINK_FLAGS=/INCREMENTAL:NO /NOLOGO
+IF %CONFIG%==D set LINK_FLAGS=%LINK_FLAGS% /DEBUG:FULL
+IF %CONFIG%==R set LINK_FLAGS=%LINK_FLAGS%
 
-cl %C_FLAGS% cgame.c /link %LINK_FLAGS%
-if "%1"=="run" if exist cgame.exe cgame.exe
+IF "%1"=="clean" (
+ IF exist *.pch DEL *.pch
+ IF EXIST *.obj DEL *.obj
+ IF EXIST *.lib DEL *.lib
+ IF EXIST *.pdb DEL *.pdb
+ IF EXIST *.exe DEL *.exe
+)
 
-if exist *.obj del *.obj
-if exist *.lib del *.lib
-if exist *.exp del *.exp
-if exist vc140.pdb del vc140.pdb
+IF NOT "%1"=="hlsl" (
+ IF EXIST %NAME%.exe DEL %NAME%.exe
+ cl %C_FLAGS% %NAME%.c /link %LINK_FLAGS%
+ IF "%1"=="run" IF EXIST %NAME%.exe %NAME%.exe
+)
+
+IF EXIST *.obj DEL *.obj
+IF EXIST %NAME%.lib DEL %NAME%.lib
+IF EXIST *.exp DEL *.exp
+IF EXIST vc140.pdb DEL vc140.pdb
--- cgame.c
+++ cgame.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#define COBJMACROS
+#include "d3d12.h"
+#include <dxgi1_4.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxguid.lib")
+
+__declspec(dllexport) extern const UINT D3D12SDKVersion = 614;
+__declspec(dllexport) extern const char *D3D12SDKPath = ".\\d3d12\\";
 
 int main(void) {
     printf("AAAAAAAAAAAAAA\n");
+
+    ID3D12Device13 *device = NULL;
+    D3D12CreateDevice(NULL, D3D_FEATURE_LEVEL_11_1, &IID_ID3D12Device13, &device);
+    assert(device);
+    ID3D12Device13_Release(device);
+
+    printf("BBBBBBBBBBBBBB\n");
     return 0;
 }
