d2ff8aec9db843644af3fe6793b820cba487c659
Michal Ziulek <michalziulek@gmail.com>
2024-08-15 20:28:59 +0200

    update

--- src/pch/pch.h
+++ src/pch/pch.h
@@ -6,7 +6,6 @@
 #define COBJMACROS
 #include "d3d12.h"
 #include <dxgi1_6.h>
-#include "cobjmacros.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,3 +37,15 @@
         (obj) = NULL; \
     } \
 } while(0)
+
+#undef ID3D12Device14_CreateCommandQueue
+#define ID3D12Device14_CreateCommandQueue(This,...)	\
+    ( (This)->lpVtbl -> CreateCommandQueue(This,__VA_ARGS__) ) 
+
+#undef IDXGIFactory7_CreateSwapChainForHwnd
+#define IDXGIFactory7_CreateSwapChainForHwnd(This,...)	\
+    ( (This)->lpVtbl -> CreateSwapChainForHwnd(This,__VA_ARGS__) ) 
+
+#undef ID3D12Device14_CreateDescriptorHeap
+#define ID3D12Device14_CreateDescriptorHeap(This,...)	\
+    ( (This)->lpVtbl -> CreateDescriptorHeap(This,__VA_ARGS__) )
