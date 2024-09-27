2f801e6a5517c3a65dd7d32d9da8fb9bade041db
Michal Ziulek <michalziulek@gmail.com>
2024-09-27 14:04:06 +0200

    audio: progress

--- src/audio.c
+++ src/audio.c
@@ -11,6 +11,67 @@ static const WAVEFORMATEX g_optimal_fmt = {
   .cbSize = sizeof(WAVEFORMATEX),
 };
 
+//
+// Pooled source voices (Psv) callback functions
+//
+static void
+Psv_OnVoiceProcessingPassStart(IXAudio2VoiceCallback *self, UINT32 BytesRequired)
+{
+  (void)self; (void)BytesRequired;
+}
+
+static void
+Psv_OnVoiceProcessingPassEnd(IXAudio2VoiceCallback *self)
+{
+  (void)self;
+}
+
+static void
+Psv_OnStreamEnd(IXAudio2VoiceCallback *self)
+{
+  (void)self;
+}
+
+static void
+Psv_OnBufferStart(IXAudio2VoiceCallback *self, void *buffer_ctx)
+{
+  (void)self; (void)buffer_ctx;
+}
+
+static void
+Psv_OnBufferEnd(IXAudio2VoiceCallback *self, void *buffer_ctx)
+{
+  (void)self;
+  IXAudio2SourceVoice *voice = (IXAudio2SourceVoice *)buffer_ctx;
+  assert(voice);
+  IXAudio2SourceVoice_Stop(voice, 0, XAUDIO2_COMMIT_NOW);
+}
+
+static void
+Psv_OnLoopEnd(IXAudio2VoiceCallback *self, void *buffer_ctx)
+{
+  (void)self; (void)buffer_ctx;
+}
+
+static void
+Psv_OnVoiceError(IXAudio2VoiceCallback *self, void *buffer_ctx, HRESULT error)
+{
+  (void)self; (void)buffer_ctx; (void)error;
+}
+
+static IXAudio2VoiceCallbackVtbl g_psv_cb_vtbl = {
+  .OnVoiceProcessingPassStart = Psv_OnVoiceProcessingPassStart,
+  .OnVoiceProcessingPassEnd = Psv_OnVoiceProcessingPassEnd,
+  .OnStreamEnd = Psv_OnStreamEnd,
+  .OnBufferStart = Psv_OnBufferStart,
+  .OnBufferEnd = Psv_OnBufferEnd,
+  .OnLoopEnd = Psv_OnLoopEnd,
+  .OnVoiceError = Psv_OnVoiceError,
+};
+static IXAudio2VoiceCallback g_psv_cb = {
+  .lpVtbl = &g_psv_cb_vtbl,
+};
+
 void aud_init_context(AudContext *aud)
 {
   assert(aud && aud->engine == NULL);
@@ -61,7 +122,8 @@ void aud_init_context(AudContext *aud)
 
   for (uint32_t i = 0; i < AUD_MAX_SOURCE_VOICES; ++i) {
     if (FAILED(IXAudio2_CreateSourceVoice(aud->engine, &aud->source_voices[i],
-      &g_optimal_fmt, 0, XAUDIO2_DEFAULT_FREQ_RATIO, NULL, NULL, NULL)))
+      &g_optimal_fmt, 0, XAUDIO2_DEFAULT_FREQ_RATIO,
+      &g_psv_cb, NULL, NULL)))
     {
       LOG("[audio] Failed to create source voice. Continuing without sound.");
       aud_deinit_context(aud);
@@ -75,6 +137,12 @@ void aud_deinit_context(AudContext *aud)
 {
   assert(aud);
   if (aud->engine) IXAudio2_StopEngine(aud->engine);
+  for (uint32_t i = 0; i < AUD_MAX_SOURCE_VOICES; ++i) {
+    if (aud->source_voices[i]) {
+      IXAudio2SourceVoice_DestroyVoice(aud->source_voices[i]);
+      aud->source_voices[i] = NULL;
+    }
+  }
   if (aud->mastering_voice) {
     IXAudio2MasteringVoice_DestroyVoice(aud->mastering_voice);
     aud->mastering_voice = NULL;
